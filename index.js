0a74a26d2046a9350fc12fd71a4a0fb5227c2f71
Sankha Narayan Guria <sankha93@gmail.com>
2015-11-06 01:52:37 +0530

    http methods forwarding

--- index.js
+++ index.js
@@ -6,11 +6,44 @@ var server = http.createServer(function(req, res) {
 
 var io = require('socket.io')(server);
 
+var httpRequest = function(method, path, body, callback) {
+  console.log(method.toUpperCase() + ':', path);
+  var options = {
+    host: 'localhost',
+    port: 4444,
+    method: method,
+    path: path
+  }
+  var req = http.request(options, function(res) {
+    var data = '';
+
+    res.on('data', function(chunk) {
+      data += chunk;
+    });
+
+    res.on('end', function() {
+      console.log('RESPONSE:', data);
+      callback(data);
+    });
+  });
+
+  req.on('error', function(err) {
+    console.log('ERROR: ' + e.message);
+  });
+
+  if (typeof body !== 'undefined') {
+    req.write(body);
+  }
+  req.end();
+}
+
 io.on('connection', function(socket){
   console.log('Accepted incoming connection.');
 
   socket.on('get', function(msg) {
-
+    httpRequest('get', msg.path, undefined, function(data) {
+      socket.emit('response', data);
+    });
   });
 
   socket.on('post', function(msg) {
--- test/tests.js
+++ test/tests.js
@@ -2,11 +2,23 @@ var io = require('socket.io-client'),
     assert = require('assert');
 
 describe('Server', function() {
-  it('should emit connect on successful connection', function(done) {
-    var socket = io.connect('http://localhost:4000');
-    socket.on('connect', function() {
-      socket.disconnect();
-      done();
+  describe('HTTP Requests', function() {
+    var socket;
+
+    before(function(done) {
+      socket = io.connect('http://localhost:4000');
+      socket.on('connect', function() {
+        done();
+      });
+    });
+
+    it('should forward a get request', function(done) {
+      socket.emit('get', { path: '/wd/hub/status' });
+      socket.on('response', function(data) {
+        var obj = JSON.parse(data);
+        assert.equal(obj.status, 0);
+        done();
+      });
     });
   });
 });
